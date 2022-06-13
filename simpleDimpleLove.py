858852e762734c4796f7f5ef505199d2dfa3011c
Davis Arrizqi Putra Mandiri <85699695+davisapplication@users.noreply.github.com>
2022-06-13 14:30:50 +0700

    simpleDimpleLove

---
+++ simpleDimpleLove.py
@@ -0,0 +1,9 @@
+import turtle
+from turtle import *
+
+def love():
+    fillcolor('red');begin_fill();
+    left(50);forward(133);circle(50,200);right(140);circle(50,200);
+    forward(133);end_fill();mainloop()
+
+love()
